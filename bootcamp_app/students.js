c98aa9f11b0fb74b2af342c95dcf16ee6a119f30
Yebbenbe <lmtpox@gmail.com>
2023-12-14 10:52:46 -0400

    corrects query name

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,33 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
-LIMIT 5;
-`)
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = $1
+LIMIT $2;
+`, [cohortName, limit])
 .then(res => {
-  console.log(res);
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
+
+
+pool.query(`
+  SELECT students.id, students.name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  LIMIT $2;
+`, [cohortName, limit])
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
