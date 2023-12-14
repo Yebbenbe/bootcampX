24d0324a8f8328e962c2ab8278c59d5945a6e8b2
Yebbenbe <lmtpox@gmail.com>
2023-12-14 11:14:17 -0400

    no change

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,20 +11,6 @@ const cohortName = process.argv[2];
 const limit = process.argv[3];
 
 pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = $1
-LIMIT $2;
-`, [cohortName, limit])
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
-
-
-pool.query(`
   SELECT students.id, students.name, cohorts.name as cohort_name
   FROM students
   JOIN cohorts ON students.cohort_id = cohorts.id
